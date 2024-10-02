ac71a0ab4207efed3f45d76495be77f9b77a2173
Wooryong <lwr0829@gmail.com>
2024-10-02 15:39:54 +0900

    Test

---
+++ Test01/Test01/main.c
@@ -0,0 +1,19 @@
+/*
+ * Test01.c
+ *
+ * Created: 2024-10-02 오후 12:17:17
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) // 무한 루프 형태 (전원이 공급되는 한 main 함수는 종료되선 안된다)
+    {
+		
+    }
+}
+
